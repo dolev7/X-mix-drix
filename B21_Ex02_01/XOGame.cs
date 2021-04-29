faed10690b049dea97e32c918748f2777fe05ce8
dolev7 <d.dolev7@gmail.com>
2021-04-29 19:46:10 +0300

    no constants class

--- B21_Ex02_01/XOGame.cs
+++ B21_Ex02_01/XOGame.cs
@@ -8,13 +8,10 @@ namespace B21_Ex02_01
 {
     public class XOGame
     {
-        public class Constants
-        {
-            public const int MAX_BOARD_SIZE = 9;
-            public const int MIN_BOARD_SIZE = 3;
-            public const int GAME_MODE_OPTION1 = 1;
-            public const int GAME_MODE_OPTION2 = 2;
-        }
+        private const int MAX_BOARD_SIZE = 9;
+        private const int MIN_BOARD_SIZE = 3;
+        private const int GAME_MODE_OPTION1 = 1;
+        private const int GAME_MODE_OPTION2 = 2;
 
         private bool m_IsGameActive;
         private bool? m_IsGameAgainstComputer;
@@ -29,12 +26,12 @@ namespace B21_Ex02_01
         public void initGame()
         {
             UIMachine.printMessageToUser("Please enter size of XO matrix");
-            int boardSize = UserInputValidator.getValidNumFromUser(Constants.MIN_BOARD_SIZE, Constants.MAX_BOARD_SIZE); 
+            int boardSize = UserInputValidator.getValidNumFromUser(MIN_BOARD_SIZE, MAX_BOARD_SIZE); 
              m_Board = new Board(boardSize);
             UIMachine.printMessageToUser("Please choose the game mode");
             UIMachine.printMessageToUser("For XO game against the computer - press 1, For XO game against a human player - press 2");
-            int usersChoiceOfGameMode = UserInputValidator.getValidNumFromUser(Constants.GAME_MODE_OPTION1, Constants.GAME_MODE_OPTION2);
-            if (usersChoiceOfGameMode==Constants.GAME_MODE_OPTION1)
+            int usersChoiceOfGameMode = UserInputValidator.getValidNumFromUser(GAME_MODE_OPTION1, GAME_MODE_OPTION2);
+            if (usersChoiceOfGameMode == GAME_MODE_OPTION1)
             {
                 m_IsGameAgainstComputer = true;
             }
