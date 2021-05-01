5d29ab532e8f3d4b95b89441f26e96452504dee6
dolev7 <d.dolev7@gmail.com>
2021-05-01 10:52:41 +0300

    boardDrawer

---
+++ B21_Ex02_01/UserInputValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B21_Ex02_01
+{
+    class UserInputValidator
+    {
+        public static int getValidNumFromUser(int i_MinValue, int i_MaxValue)
+        {
+            bool isValid = false;
+            int validNum = 0;
+            string userInput;
+
+            while (!isValid)
+            {
+                userInput = UIMachine.getDataFromUser();
+                isValid = int.TryParse(userInput, out validNum);
+                if ((validNum >= i_MinValue) && (validNum <= i_MaxValue))
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    isValid = false;
+                    UIMachine.printMessageToUser("Please enter a correct number");
+                }
+            }
+
+            return validNum;
+
+        }
+    }
+}
