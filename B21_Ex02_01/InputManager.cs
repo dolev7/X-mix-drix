89f13e717ceb793d1d4e431002c555fb676567df
dolev7 <d.dolev7@gmail.com>
2021-05-05 00:22:24 +0300

    need to implement q and fix everything

---
+++ B21_Ex02_01/InputManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B21_Ex02_01
+{
+    class InputManager
+    {
+        public static string getDataFromUser()
+        {
+            string userData;
+            userData = Console.ReadLine();
+            return userData;
+        }
+        public static int getValidNumFromUser(int i_MinValue, int i_MaxValue)
+        {
+            bool isValid = false;
+            int validNum = 0;
+            while (!isValid)
+            {
+                string userInput = getDataFromUser();
+                isValid = int.TryParse(userInput, out validNum);
+                if ((validNum >= i_MinValue) && (validNum <= i_MaxValue))
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    isValid = false;
+                    OutputManager.printMessageToUser("Please enter a correct number");
+                }
+                
+            }
+
+            return validNum;
+        }
+    }
+}
---
+++ B21_Ex02_01/UI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B21_Ex02_01
+{
+    public class UI
+    {
+        public const int k_MaxBoardSize = 9;
+        public const int k_MinBoardSize = 3;
+        public const int k_GameModeOptionOne = 1;
+        public const int k_GameModeOptionTwo = 2;
+        public const char k_SymbolOne = 'X';
+        public const char k_SymbolTwo = 'O';
+        public static void RunGame()
+        {
+            OutputManager.printMessageToUser("Please enter size of XO board");
+            int boardSize = InputManager.getValidNumFromUser(k_MinBoardSize, k_MaxBoardSize);
+            OutputManager.printMessageToUser
+(@"Please choose the game mode:
+For XO game against the computer - press 1
+For XO game against a human player - press 2");
+            int usersChoiceOfGameMode = InputManager.getValidNumFromUser(k_GameModeOptionOne, k_GameModeOptionTwo);
+
+            GameManager xogame = new GameManager();
+            xogame.InitGame(boardSize, usersChoiceOfGameMode);
+            OutputManager.drawBoard(xogame.GameBoard);
+            xogame.PlayGame();
+        }
+        public static int GetRowFromPlayer(int i_BoardSize)
+        {
+            OutputManager.printMessageToUser("Please enter row number");
+            return InputManager.getValidNumFromUser(1, i_BoardSize);            
+        }
+        public static int GetColFromPlayer(int i_BoardSize)
+        {
+            OutputManager.printMessageToUser("Please enter col number");
+            return InputManager.getValidNumFromUser(1, i_BoardSize);
+        }
+
+    }
+}
